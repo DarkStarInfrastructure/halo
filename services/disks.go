0019e891c8a73922aaea8ffd85f6689f1a3b3b7d
JiangYu <lnsyyj@hotmail.com>
2022-11-20 00:06:47 +0800

    add lsblk interface
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- services/disks.go
+++ services/disks.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"encoding/json"
 	"fmt"
 	"halo/utils"
 	"net/http"
@@ -8,25 +9,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Disk struct {
-	// The name of the disk.
-	Name string `json:"name"`
+type Disks struct {
+	Blockdevices []Blockdevices `json:"blockdevices"`
 }
 
-type Disks struct {
-	Disks []Disk `json:"disks"`
+type Blockdevices struct {
+	Name       string      `json:"name"`
+	MajMin     string      `json:"maj:min"`
+	Rm         string      `json:"rm"`
+	Size       string      `json:"size"`
+	Ro         string      `json:"ro"`
+	Type       string      `json:"type"`
+	Mountpoint interface{} `json:"mountpoint"`
 }
 
 func ListAllDisks(c *gin.Context) {
 
-	result, err := utils.ExecCommand("lsblk", "-d", "--json")
+	// -d, --nodeps         don't print slaves or holders
+	// -O, --output-all     output all columns
+	// -b, --bytes          print SIZE in bytes rather than in human readable format
+	// -J, --json           use JSON output format
+	result, err := utils.ExecCommand("lsblk", "-d", "-O", "-b", "--json")
 	if err != nil {
 		fmt.Fprintln(gin.DefaultWriter, err.Error())
+		c.JSON(http.StatusInternalServerError, err.Error())
 	}
 
 	fmt.Fprintln(gin.DefaultWriter, result)
 
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, gin.H{"result": json.RawMessage(result)})
 }
 
 func GetDiskDetails(c *gin.Context) {
--- utils/disks.go
+++ utils/disks.go
@@ -4,6 +4,10 @@ import "os/exec"
 
 func ExecCommand(command string, args ...string) (string, error) {
 	cmd := exec.Command(command, args...)
+	stdout, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
 
-	return cmd.Output()
+	return string(stdout), nil
 }
